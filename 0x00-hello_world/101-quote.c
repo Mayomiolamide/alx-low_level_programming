bfaf1465048b766cebe4c15bc82f792afc4dc0b4
Mayor <akintayomayomiolamide@gmail.com>
2022-09-08 08:37:23 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,9 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (notsucess)
+ * Return: Always 1 (notsucces)
  */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+int main(void) { write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
