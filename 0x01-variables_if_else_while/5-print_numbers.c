97bb47913bdd54d5df0d819b348b8a822df9db7f
Mayor <akintayomayomiolamide@gmail.com>
2022-09-09 08:22:20 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 	
-	for (i =0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 
 	{
 		printf("%i", i);
@@ -17,6 +17,6 @@ int main(void)
 
 	printf("\n");
 
-		return(0);
+		return (0);
 
 }
