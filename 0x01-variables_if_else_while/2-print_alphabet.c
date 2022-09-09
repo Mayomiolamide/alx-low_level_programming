99dbb0811724a87b6bb6328409459f3c355e451a
Mayor <akintayomayomiolamide@gmail.com>
2022-09-09 06:52:06 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet= 'a'; alphabeth <= 'z'; alphabeth+++)
+{
+putchar(alphabeth);
+}
+
+ptchar('\n');
+
+return (0);
+
+}
