b471887f38b205fdb593c378f8b89c7c4c65b13b
Mayor <akintayomayomiolamide@gmail.com>
2022-09-16 04:52:05 -0700

    tri

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (inc1 =1; inc1 <= size; inc1++)
+		for (inc1 = 1; inc1 <= size; inc1++)
 		{
 			for ((inc2 = size - inc1); inc2 > 0; inc2++)
 			{
@@ -22,7 +22,7 @@ void print_triangle(int size)
 
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
 
 			if (inc1 == size)
@@ -30,9 +30,9 @@ void print_triangle(int size)
 				continue;
 			}
 
-			putchar('\n');
+			_putchar('\n');
 		}
 
 	}
-	putchar('\n')
+	_putchar('\n')
 }
