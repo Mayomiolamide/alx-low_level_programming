2161d18b24684433b97dec28c3113e85fe3f1a17
Mayor <akintayomayomiolamide@gmail.com>
2022-09-19 08:00:08 -0700

    string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,13 +12,14 @@ void rev_string(char *s)
 {
 	int i,len,temp;
 
-	len = strlen(str1);
+	len = strlen(s);
 
 	for (i = 0; i < len / 2; i++)
 	{
-		temp =str1[i];
-		str1[i] = str1[len - i - 1];
+		temp =s[i];
+		s[i] = s[len - i - 1];
 
-		str1[len - i 1] = temp;
+		s[len - i 1] = temp;
 	}
+	revstr(str)
 }
