4caf25040cb23ea5f6084b8ec8bdc38bfb4f4eb2
Mayor <akintayomayomiolamide@gmail.com>
2022-09-21 14:57:31 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void reverse_array - function to reverse a string
+ * @a: array
+ * @n: number of bytes
+ * Return: yeah
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+	
+	for (index =n - 1; index > n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
