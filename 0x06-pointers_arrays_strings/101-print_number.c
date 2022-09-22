20d51ccd9a953203dcbfd0239e9945c62ee06cc3
Mayor <akintayomayomiolamide@gmail.com>
2022-09-22 12:02:28 -0700

    number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_number - prints an iteger
- * @n: integer to nbe
- *
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
+
 void print_number(int n)
 {
 	unsigned int num = n;
