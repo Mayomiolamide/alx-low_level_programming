a59a6e6bba0163c562b9e6eee5c781a891b24d8a
Mayor <akintayomayomiolamide@gmail.com>
2022-09-26 15:22:50 -0700

    set

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value
+ * @s: pointer to change
+ * @to: string to change pointer to char
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
