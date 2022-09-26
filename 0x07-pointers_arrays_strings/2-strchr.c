8fa4ca9b28c94daf155ffa44835d84b026597686
Mayor <akintayomayomiolamide@gmail.com>
2022-09-26 13:39:24 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - fucntion to locate string
+ * @c: variable c
+ * @s: variable s
+ *
+ * Return: a pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s - i); i++)
+	{
+		if (*(s + i) == c)
+	}
+	return (s + i);
+}
