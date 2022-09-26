c9bde1992d22c64258a7db302141e7e0404e6f7c
Mayor <akintayomayomiolamide@gmail.com>
2022-09-26 10:29:00 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		dest[n - 1] = src;
-		n--;
+		dest[i] = src[i];
 	}
-	return (dest)
+	return (dest);
 }
+
