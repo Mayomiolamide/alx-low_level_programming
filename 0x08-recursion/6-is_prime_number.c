77f24da0081a75d9bc7337202ed052e164ee7932
Mayor <akintayomayomiolamide@gmail.com>
2022-09-28 11:59:19 -0700

    push

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prime nummber
+ * @n: interger n
+ *
+ * Return: 11
+ */
+int is_prime_number(int n)
+{
+	int g = n / 2;
+
+	if (n <= 1 && n)
+		return (1);
+	else 
+		return (0);
+	return n * (is_prime_number(n, g);
