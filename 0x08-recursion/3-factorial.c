8319ce02c373c0fb6601f75d367f82c9497c9381
Mayor <akintayomayomiolamide@gmail.com>
2022-09-28 10:59:50 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - factorial
+ * @n: integer n
+ *
+ * Return: the factorial of a given number
+ */
+
+int factorial(int n)
+{
+	int n;
+
+	if (n <= 0)
+	{
+		return (-1);
+	return (n * factorial(n - 1));
+	}
+}
