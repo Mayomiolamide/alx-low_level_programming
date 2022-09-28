b013cc543d6c1e3483b72df8ffe25bd95bab17b3
Mayor <akintayomayomiolamide@gmail.com>
2022-09-28 14:45:06 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,8 +11,9 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
 		return (n);
-	return (_sqrt(0, n));
+	return (_sqrt_recursion(0, n));
 }
+
 /**
  * _sqrt - returns the square
  * @n: test num
