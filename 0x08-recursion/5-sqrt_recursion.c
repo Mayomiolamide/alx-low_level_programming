284fd403112e899f3845e996da8f40b80c6634a8
Mayor <akintayomayomiolamide@gmail.com>
2022-09-28 11:35:34 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - recursion
+ * @n: variable n
+ *
+ * Return: -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n ==  1 || n == 0)
+		return (n);
+	return (_sqrt(0, n));
+
+	else if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x)
