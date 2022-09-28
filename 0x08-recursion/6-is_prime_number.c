82259c969b75ecfabdc10220df372f3a7e128ea8
Mayor <akintayomayomiolamide@gmail.com>
2022-09-28 12:06:05 -0700

    push

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,24 @@ int is_prime_number(int n)
 	int g = n / 2;
 
 	if (n <= 1)
-		retrun(0);
-	return (is_prime_number(n, g));
+		return (0);
+	return (is_prime(n, g));
+}
+
+/**
+ * is_prime -returns
+ * @n: odh
+ * @start: snshs
+ *
+ * Return: 1
+ */
+
+int is_prime(int n, int start)
+{
+	if (start <= 1)
+		return (1);
+	else if (n % start == 0)
+		return (0);
+	return(is_prime(n, start - 1));
 }
 			
