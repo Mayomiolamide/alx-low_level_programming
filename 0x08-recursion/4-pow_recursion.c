39583a33441976f9107dc2fb5e0d58b17ded12d2
Mayor <akintayomayomiolamide@gmail.com>
2022-09-28 11:24:51 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,8 @@
  * _pow_recursion - power recursion
  * @x: variable x
  * @y: variable y
+ *
+ * Return: 1 if 1 is lower than 0
  */
 
 int _pow_recursion(int x, int y)
@@ -12,5 +14,5 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y -1));
+	return (x * _pow_recursion(x, y - 1));
 }
