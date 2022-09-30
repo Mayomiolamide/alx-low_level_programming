13dc8e5a2fdb91e083712d4253810266bb5c1734
Mayor <akintayomayomiolamide@gmail.com>
2022-09-30 12:09:19 -0700

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0);
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			return (0);
+		}
+	}
+}
