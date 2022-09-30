ba0f1c33a2b4b69f4fd705455f250c8d635e0ee4
Mayor <akintayomayomiolamide@gmail.com>
2022-09-30 10:57:41 -0700

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
-int main(int argc, argv)
+/**
+ * main - main program
+ * @argc: argument cat
+ * @argv: argument bersion
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv)
 {
 	_putchar("argc: %d\n", argc);
+	return (0);
 }
