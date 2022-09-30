55829bffa29d01af8e3c55f6f3690d2f77ccdd6d
Mayor <akintayomayomiolamide@gmail.com>
2022-09-30 12:59:51 -0700

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -func
+ * @argc: length of
+ * @argv: hshs
+ * Return: 0 awua
+ */
+
+int main(int argc, char *argv[])
+{
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	position = total = change = aux = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]);
+
+	if (total <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+}
