aefafd01968b952f8b7ea07c0597231673089177
Mayor <akintayomayomiolamide@gmail.com>
2022-10-04 03:00:35 -0700

    args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * argstostr - args
+ * @ac: ac
+ * @len: len
+ * @str: str
+ * @**av: av
+ * Reurn: null if ac is 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int len = 0
+
+		if (str != NULL)
+		{
+			while (str[len])
+				len++;
+		}
+	return(len);
+}
