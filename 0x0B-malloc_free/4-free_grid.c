577ce7bf9fa969ef891a49807672af712a9bddb3
Mayor <akintayomayomiolamide@gmail.com>
2022-10-04 02:50:02 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - free
+ * @grid: grid
+ * @height: heght
+ * Return: null 
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
