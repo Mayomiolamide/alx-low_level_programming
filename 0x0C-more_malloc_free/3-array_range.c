359c4d06e67547c9caff02126b8247bbc5d7fa4f
Mayor <akintayomayomiolamide@gmail.com>
2022-10-06 13:10:57 -0700

    arange

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,10 +9,10 @@
 int *array_range(int min, int max)
 {
 	int *arr, i = 0, t = min;
-	
+
 	if (min > max)
 		return (0);
-	arr = malloc(max - min + 1) * sizeof(int));
+	arr = malloc((max - min + 1) * sizeof(int));
 
 	if (!arr)
 		return (0);
