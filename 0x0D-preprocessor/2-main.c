107b8fc161749fdd0241de919a7b5f231584f065
Mayor <akintayomayomiolamide@gmail.com>
2022-10-10 13:04:27 -0700

    main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the name of the file program
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
