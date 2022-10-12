08584d9a1c7161f763f07746780255c9764460b4
Mayor <akintayomayomiolamide@gmail.com>
2022-10-12 14:39:34 -0700

    function to perform arithemetic operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - unction to add 
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to subtract
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: a -b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function to multiply
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function to divide
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function to print remainder
+ * @a: variable a 
+ * @b: variable b
+ *
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
